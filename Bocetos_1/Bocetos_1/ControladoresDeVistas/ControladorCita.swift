36b271862db88e60257f7181f2fc7b91485635cf
alumno <alumno@PC-V202-24.uacj.mx>
2024-09-20 10:40:54 -0600

    [Actualizado] se actualizaron las vistas y se agrego un nuevo codigo

---
+++ Bocetos_1/Bocetos_1/ControladoresDeVistas/ControladorCita.swift
@@ -0,0 +1,18 @@
+//
+//  ControladorCita.swift
+//  Bocetos_1
+//
+//  Created by alumno on 9/20/24.
+//
+
+import SwiftUI
+
+struct ControladorCita: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ControladorCita()
+}
