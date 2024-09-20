ada02b6369d32fe2ca40d8bbfd56bc2a73adf52f
alumno <alumno@PC-V202-24.uacj.mx>
2024-09-20 10:44:07 -0600

    [AGREGAR] se agregaron codigos y actualizaron las vistas

--- Bocetos_1/Bocetos_1/ControladoresDeVistas/ControladorCita.swift
+++ Bocetos_1/Bocetos_1/ControladoresDeVistas/ControladorCita.swift
@@ -1,18 +1,45 @@
 //
-//  ControladorCita.swift
+//  ViewController.swift
 //  Bocetos_1
 //
-//  Created by alumno on 9/20/24.
+//  Created by alumno on 9/18/24.
 //
 
-import SwiftUI
+import UIKit
 
-struct ControladorCita: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class ControladorVistaCita: UIViewController {
+    @IBOutlet weak var nombre_de_quien_lo_dijo: UILabel!
+    @IBOutlet weak var que_dijo_que: UILabel!
+    
+    var texto: String
+    
+    required init?(coder: NSCoder)
+    {
+        super.init(coder: coder)
+        print("Algo paso por aqui")
     }
+    
+    init(titulo_secundario: String, coder: String)
+    {
+        texto = titulo_secundario
+        
+        super.init(coder: coder)!
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Me estoy ejecutando")
+        
+        nombre_de_quien_lo_dijo.text = texto
+        
+    }
+    
+    @IBSegueAction func moviendo_pantalla(_ coder: NSCoder) -> ControladorVistaCita? {
+        return ControladorVistaCita()
+    }
+    
+
 }
 
-#Preview {
-    ControladorCita()
-}
