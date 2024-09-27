1524baec6d00198351320b94648b82b1ea528a5d
alumno <alumno@PC-V202-24.uacj.mx>
2024-09-27 10:46:08 -0600

    [AGREGADO] se agregaron los campos para insertar texto

---
+++ Bocetos_1/Bocetos_1/ControladoresDeVistas/ControlGeneradorCita.swift
@@ -0,0 +1,30 @@
+//
+//  ControlGeneradorCita.swift
+//  Bocetos_1
+//
+//  Created by alumno on 9/27/24.
+//
+
+import Foundation
+import UIKit
+
+class ControlPantallaAgregarCita: UIViewController
+{
+    var quien_lo_dice: String = ""
+    var que_dice: String = ""
+    
+    @IBOutlet weak var quien_lo_dijo_view: UITextField!
+    @IBOutlet weak var que_dijo_view: UITextField!
+    @IBAction func agregar_cita_nueva(_ sender: Any)
+    {
+        cita_creada = Cita(quien_lo_dijo: quien_lo_dijo_view.text!,
+                           muro_de_texto: que_dijo_view.text!)
+    }
+    
+    var cita_creada: Cita? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
--- Bocetos_1/Bocetos_1/ViewController.swift
+++ Bocetos_1/Bocetos_1/ViewController.swift
@@ -11,22 +11,39 @@ class ViewController: UIViewController {
     
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Barry B. Benson", muro_de_texto: "Te gusta el jazz?~")
     var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
-
+    
+    @IBOutlet weak var labelcito: UILabel!
+    
     override func viewDidLoad() {
         citas_disponibles.generar_citas_falsas()
         super.viewDidLoad()
+        
+        actualizar_cantidad()
         // Do any additional setup after loading the view.
         
         let valor = Int.random(in: 0...100)
         Double.random(in: 0...100)
         
     }
-
+    
+    func actualizar_cantidad()
+    {
+        labelcito.text = String(citas_disponibles.citas_creadas.count)
+    }
+    
     @IBSegueAction func poner_la_cosa_cobre_la_cosa(_ coder: NSCoder) -> OtroControlador? {
         return OtroControlador(cita_para_cita: citas_disponibles.obtener_citas_aleatorias(), coder: coder)
     }
-    @IBSegueAction func guardar_la_cosa_con_las_demas(_ coder: NSCoder) -> OtroControladorMas? {
-        return <#OtroControladorMas(coder: coder)#>
+    
+    @IBAction func volver_a_la_pantalla_inicio(segue: UIStoryboardSegue)
+    {
+        if let pantalla_agregar_citas = segue.source as?
+                ControlPantallaAgregarCita
+        {
+            citas_disponibles.agregar_cita(pantalla_agregar_citas.cita_creada!)
+        }
+        
+        actualizar_cantidad()
     }
 }
 
