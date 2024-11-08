5d26fca239e16207fdd243d24f6f88ef7d37e4a5
alumno <alumno@PC-V202-19.uacj.mx>
2024-11-08 10:48:40 -0700

    [GALERIA] ya se empezo a trabajar la galeria

---
+++ ProyectoFinal/ProyectoFinalIOS/Vistas/VistaDeGaleria/ControladorVistaGaleria.swift
@@ -0,0 +1,14 @@
+//
+//  ControladorVistaGaleria.swift
+//  ProyectoFinalIOS
+//
+//  Created by alumno on 11/8/24.
+//
+
+import Foundation
+import UIKit
+
+class ControladorVistaGaleria : UIViewController
+{
+    
+}
